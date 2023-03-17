e52aa089b03570cf8adaa78d99c18a904ef595d2
Revsri <117416155+Revsri@users.noreply.github.com>
2023-03-16 17:56:44 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+//https://teachablemachine.withgoogle.com/models/O-KIMFDpn/
+
+function startClassification()
+{
+    navigator.mediaDevices.getUserMedia({ audio: true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/O-KIMFDpn/model.json', modelReady);
+}
+
+function modelReady(){
+    classifier.classify(gotResults);
+}
